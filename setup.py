ea68c6527957700eb738348e13c9051c899f9720
Harvey Bastidas <harveybc@ingeni-us.com>
2024-12-30 17:27:35 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='trading-signal',
-    version='0.1.1',
+    version='0.1.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
